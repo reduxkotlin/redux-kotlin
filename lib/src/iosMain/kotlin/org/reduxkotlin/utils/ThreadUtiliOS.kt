442414f44a17efe72bf2532a7851d640d682b80a
Patrick Jackson <patrick.jackson@ableto.com>
2019-12-06 17:18:00 -0500

    add macos

--- lib/src/iosMain/kotlin/org/reduxkotlin/utils/ThreadUtiliOS.kt
+++ lib/src/iosMain/kotlin/org/reduxkotlin/utils/ThreadUtiliOS.kt
@@ -1,4 +1,4 @@
 package org.reduxkotlin.utils
 import platform.Foundation.NSThread.Companion.currentThread
 
-actual fun getThreadName(): String = currentThread.name ?: "Unknown"
+actual fun getThreadName(): String = currentThread.name ?: UNKNOWN_THREAD_NAME
---
+++ lib/src/winMain/kotlin/org/reduxkotlin/utils/ThreadUtilWin.kt
@@ -0,0 +1,4 @@
+package org.reduxkotlin.utils
+
+//TODO figure out how to pull in win deps
+actual fun getThreadName(): String = UNKNOWN_THREAD_NAME
