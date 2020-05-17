eaad25b146b776d9aa3036a59b42296bc5116459
Patrick Jackson <patrick.jackson@ableto.com>
2020-05-17 19:23:15 -0400

    remove combineEnhancers

--- lib/src/commonMain/kotlin/org/reduxkotlin/CombineEnhancers.kt
+++
@@ -1,8 +0,0 @@
-package org.reduxkotlin
-
-
-fun<State> combineEnhancers(vararg enhancers: StoreEnhancer<State>): StoreEnhancer<State> =
-        { storeCreator ->
-            compose(enhancers.map { it })(storeCreator)
-        }
-
