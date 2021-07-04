c99ea875c142ac3c98fcb08672060612a24c7f88
Alex <a.shepeliev@gmail.com>
2021-07-04 15:41:31 +0300

    Downgrade Kotlin version to 1.5.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -30,7 +30,7 @@ object Versions {
 
     const val dokka_gradle_plugin: String = "0.9.17" // available: "0.11.0-dev-59"
 
-    const val org_jetbrains_kotlin: String = "1.5.20"
+    const val org_jetbrains_kotlin: String = "1.5.0"
 
     const val atomicfu_gradle_plugin: String = "0.16.1"
 
