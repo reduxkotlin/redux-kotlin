3623a767de747fa621cdbfd88c24614d7ecbd2b4
patjackson52 <patjackson52@gmail.com>
2026-05-26 16:46:01 -0700

    feat(rss-reader): Koin provides FeedStoreHolder instead of NanoRedux FeedStore

--- examples/rss-reader/androidApp/src/main/kotlin/com/github/jetbrains/rssreader/App.kt
+++ examples/rss-reader/androidApp/src/main/kotlin/com/github/jetbrains/rssreader/App.kt
@@ -2,7 +2,7 @@ package com.github.jetbrains.rssreader
 
 import android.app.Application
 import android.content.Context
-import com.github.jetbrains.rssreader.app.FeedStore
+import com.github.jetbrains.rssreader.app.FeedStoreHolder
 import com.github.jetbrains.rssreader.core.buildRssReader
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -19,7 +19,7 @@ class App : Application() {
 
     private val appModule = module {
         single { buildRssReader(get<Context>(), BuildConfig.DEBUG) }
-        single { FeedStore(get()) }
+        single { FeedStoreHolder() }
     }
 
     private fun initKoin() {
