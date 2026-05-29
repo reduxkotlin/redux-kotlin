90ff9067fa533fd30b08a5fb15a85ffae34b26be
patjackson52 <patjackson52@gmail.com>
2026-05-28 22:53:18 -0700

    feat(bundle): add redux-kotlin-bom platform + publishing convention

--- redux-kotlin-bom/build.gradle.kts
+++ redux-kotlin-bom/build.gradle.kts
@@ -1,1 +1,25 @@
-// scaffolded in a later task
+plugins {
+    `java-platform`
+    id("convention.publishing-platform")
+}
+
+description = "Bill of Materials aligning all redux-kotlin module versions"
+
+dependencies {
+    constraints {
+        val v = project.version.toString()
+        val g = project.group.toString()
+        api("$g:redux-kotlin:$v")
+        api("$g:redux-kotlin-threadsafe:$v")
+        api("$g:redux-kotlin-granular:$v")
+        api("$g:redux-kotlin-registry:$v")
+        api("$g:redux-kotlin-multimodel:$v")
+        api("$g:redux-kotlin-multimodel-granular:$v")
+        api("$g:redux-kotlin-compose:$v")
+        api("$g:redux-kotlin-compose-multimodel:$v")
+        api("$g:redux-kotlin-routing:$v")
+        api("$g:redux-kotlin-routing-codegen:$v")
+        api("$g:redux-kotlin-bundle:$v")
+        api("$g:redux-kotlin-bundle-compose:$v")
+    }
+}
