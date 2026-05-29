397dc168c32ad169486007ff07ebfe35c673b643
patjackson52 <patjackson52@gmail.com>
2026-05-28 22:36:33 -0700

    build(bundle): scaffold redux-kotlin-bundle aggregator

---
+++ redux-kotlin-bundle/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("convention.library-mpp-loved")
+    id("convention.publishing-mpp")
+}
+
+val hasAndroidSdk: Boolean = run {
+    val localProps = rootProject.file("local.properties")
+    val hasSdkInLocalProperties = localProps.exists() && localProps.readText().lineSequence().any {
+        it.trim().startsWith("sdk.dir=") && it.substringAfter("sdk.dir=").isNotBlank()
+    }
+    val hasSdkInEnv =
+        !System.getenv("ANDROID_HOME").isNullOrBlank() ||
+            !System.getenv("ANDROID_SDK_ROOT").isNullOrBlank()
+    hasSdkInLocalProperties || hasSdkInEnv
+}
+
+kotlin {
+    if (hasAndroidSdk) {
+        android {
+            namespace = "org.reduxkotlin.bundle"
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(project(":redux-kotlin-threadsafe"))
+                api(project(":redux-kotlin-registry"))
+                api(project(":redux-kotlin-routing"))
+                api(project(":redux-kotlin-multimodel-granular"))
+            }
+        }
+    }
+}
