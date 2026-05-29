eee11cde6b03441b6cdec09a4e4bad1e77d01a46
patjackson52 <patjackson52@gmail.com>
2026-05-29 07:35:28 -0700

    fix(bundle): update bundle-compose surface test to createConcurrentModelStore

--- redux-kotlin-bundle-compose/src/commonTest/kotlin/org/reduxkotlin/bundle/compose/BundleComposeSurfaceTest.kt
+++ redux-kotlin-bundle-compose/src/commonTest/kotlin/org/reduxkotlin/bundle/compose/BundleComposeSurfaceTest.kt
@@ -1,6 +1,6 @@
 package org.reduxkotlin.bundle.compose
 
-import org.reduxkotlin.bundle.createThreadSafeModelStore
+import org.reduxkotlin.bundle.createConcurrentModelStore
 import org.reduxkotlin.compose.StableStore
 import kotlin.test.Test
 import kotlin.test.assertTrue
@@ -10,7 +10,7 @@ class BundleComposeSurfaceTest {
     @Test
     fun base_bundle_and_compose_bindings_are_reachable() {
         // reference both so the imports are used; adapt the compose reference to the symbol you imported
-        val a: Any = ::createThreadSafeModelStore
+        val a: Any = ::createConcurrentModelStore
         val b = StableStore::class
         assertTrue(a != Unit)
         assertTrue(b.simpleName == "StableStore")
