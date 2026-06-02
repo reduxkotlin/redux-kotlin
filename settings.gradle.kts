575ac1c87f8662b1b5d699eed337453dadaf332d
patjackson52 <patjackson52@gmail.com>
2026-06-02 00:06:13 -0700

    build(devtools-inapp): scaffold Compose Multiplatform module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,6 +33,7 @@ include(
     ":redux-kotlin-compose-multimodel",
     ":redux-kotlin-devtools-core",
     ":redux-kotlin-devtools-remote",
+    ":redux-kotlin-devtools-inapp",
     ":examples:counter:common",
     ":examples:counter:android",
     ":examples:todos:common",
